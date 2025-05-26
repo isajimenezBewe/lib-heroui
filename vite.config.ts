344e9aebc2945e5b5f0c8018abdf20a1729a6159
isajimenez <isabela.jimenez@bewe.io>
2025-05-26 11:32:46 -0500

    base de la libreria

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MiLibreriaUI',
+      formats: ['es', 'umd'],
+      fileName: (format) => `mi-libreria-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'framer-motion'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'framer-motion': 'FramerMotion',
+        },
+      },
+    },
+  },
+})
